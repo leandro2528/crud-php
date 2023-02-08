d26c863537a7d4afdbd8b58089b387a8acd2e32a
Leandro Carvalho <leandrodev83@gmail.com>
2023-02-08 15:41:52 -0300

    corrigindo erro no formulário de edição

--- editar-usuario.php
+++ editar-usuario.php
@@ -8,7 +8,8 @@
 
     ?>
     <form action="?page=salvar" method="POST">
-        <input type="hidden" name="acao" value="cadastrar">
+        <input type="hidden" name="acao" value="editar">
+        <input type="hidden" name="id" value="<?php print $row->id; ?>">
         <div class="mb-3">
             <label for="">Nome</label>
             <input type="text" name="nome" value="<?php print $row->nome ?>" class="form-control">
